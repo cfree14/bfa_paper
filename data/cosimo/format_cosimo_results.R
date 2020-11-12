12e72b9b0f4dadbd2a7af22f02b52c22e765384f
Chris Free <cfree@iMac-4.local>
2020-11-12 12:41:42 -0800

    added more formatting of COSIMO data

--- data/cosimo/format_cosimo_results.R
+++ data/cosimo/format_cosimo_results.R
@@ -94,35 +94,47 @@ format_data("AqCaHigBaseScen")
 # Format other data
 ################################################################################
 
-# Read data
-data_orig <- read.csv(file.path(inputdir, "FoodConsBase.csv"), as.is=T, skip=1)
+file <- "FoodConsBase"
+format_data2 <- function(file){
+  
+  # Read data
+  infile <- paste0(file, ".csv")
+  data_orig <- read.csv(file.path(inputdir, infile), as.is=T, skip=1)
+  
+  # Format data
+  data <- data_orig %>% 
+    # Gather years
+    gather(key="year", value="value", 5:ncol(.)) %>% 
+    # Format year 
+    mutate(year=year %>% gsub("X|A", "", .) %>% as.numeric()) %>%
+    filter(!is.na(year)) %>% 
+    # Rename columns
+    rename(long_code=OUTPUT.0, description=Comment, location=Location, type=Type) %>% 
+    # Remove useless columns
+    select(-c(location, type)) %>% 
+    # Extract useful stuff from code
+    mutate(iso3 = strsplit_extract(x=long_code, split="_", which=1),
+           food_code = strsplit_extract(x=long_code, split="_", which=2),
+           code3=strsplit_extract(x=long_code, split="_", which=3),
+           code3a=strsplit_extract(x=code3, split="..", which=1),
+           code3b=strsplit_extract(x=code3, split="..", which=2)) %>% 
+    # Arrange
+    select(iso3, food_code, code3, code3a, code3b, year, everything()) %>% 
+    arrange(iso3, food_code, year) %>% 
+    # Remove useless columns
+    select(-c(code3, code3a, code3b, long_code, description))
+  
+  # Inspect data
+  str(data)
+  # table(data$location)
+  # table(data$type)
+  
+  # Export data
+  outfile <- paste0(file, "_formatted.csv")
+  write.csv(data, file=file.path(outputdir, outfile), row.names=F)
+  
+}
 
-# Format data
-data <- data_orig %>% 
-  # Remove emoty column
-  select(-X) %>% 
-  # Gather years
-  gather(key="year", value="value", 5:ncol(.)) %>% 
-  # Format year 
-  mutate(year=year %>% gsub("X|A", "", .) %>% as.numeric()) %>%
-  # Rename columns
-  rename(long_code=OUTPUT.0, description=Comment, location=Location, type=Type) %>% 
-  # Remove useless columns
-  select(-c(location, type)) %>% 
-  # Extract useful stuff from code
-  mutate(iso3 = strsplit_extract(x=long_code, split="_", which=1),
-         food_code = strsplit_extract(x=long_code, split="_", which=2),
-         code3=strsplit_extract(x=long_code, split="_", which=3),
-         code3a=strsplit_extract(x=code3, split="..", which=1),
-         code3b=strsplit_extract(x=code3, split="..", which=2)) %>% 
-  # Arrange
-  select(iso3, food_code, code3, code3a, code3b, year, everything()) %>% 
-  arrange(iso3, food_code, year) %>% 
-  # Remove useless columns
-  select(-c(code3, code3a, code3b, long_code, description))
-
-# Inspect data
-str(data)
-# table(data$location)
-# table(data$type)
+format_data2(file="FoodConsAqCaH")
+format_data2(file="FoodConsBase")
 
